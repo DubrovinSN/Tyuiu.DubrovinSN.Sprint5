ebc01efeacbb5fa38a65c78fcdc0d3058d1edd96
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-11-20 20:55:21 +0500

    Task4

---
+++ Tyuiu.DubrovinSN.Sprint5.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DubrovinSN.Sprint5.Task4.V23.Lib;
+using System.IO;
+
+namespace Tyuiu.DubrovinSN.Sprint5.Task4.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedLoad()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V23.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.DubrovinSN.Sprint5.Task4.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DubrovinSN.Sprint5.Task4.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DubrovinSN.Sprint5.Task4.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("081495cb-3565-4d00-83a8-372ce3166ce9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
